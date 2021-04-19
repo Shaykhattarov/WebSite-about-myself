4302f2c3f025d878e90c824170a63a330072b661
Ildan Shaykhattarov <ildan201@yandex.ru>
2021-04-19 18:14:29 +0300

    Новые функции

--- js/script.js
+++ js/script.js
@@ -1,22 +1,82 @@
-var rows="";
-var matrix=0;
-
-function create2DArray(rows, value){
-    var arr = new Array;
-
-    for(var i=0; i < rows; i++){
-        arr[i] = [];
+function Create2DArray(length) {
+    var arr = [];
+  
+    for (var i=0;i<length;i++) {
+       arr[i] = [];
     }
+  
+    return arr;
 }
 
-function input(){
-    rows = document.getElementById("rows").value;
-    var value = document.getElementById("matrix").value;
-    matrix = create2DArray(rows, value);
+function matrixInput(){
+    var length = document.getElementById("rows").value;
+    var str = document.getElementById("matrix").value;
+    var matrix = Create2DArray(length);
+
+    var coun = 0, i = 0, j = 0;
+    while(str.length != coun){
+        if(str[coun] != " " && str[coun] != "\n" && str[coun] != "\0") {
+            matrix[i][j] = str[coun];
+            j++;
+            coun++;
+        }
+        else if(str[coun] == "\n"){
+            i++;
+            j = 0;
+            coun++;
+        }
+        else{
+            coun++;
+        }
+    }
+
     return matrix;
 }
-input();
-console.log(matrix);
-function reflexion(){
 
+
+function reflexion(){
+    var matrix = matrixInput();
+    var val = true;
+    var startArr = matrix[0][0];
+    for(i = 0; i < matrix.length; i++){
+        if(matrix[i][i] != startArr){
+            val = false;
+            break;
+        }
+    }
+    return val;
 }
+
+
+function symmetry(){
+    var matrix = matrixInput();
+    var val = true;
+    for(i = 0; i < matrix.length; i++){
+        for( j = 0; j < matrix.length; j++){
+            if(matrix[i][j] != matrix[j][i]){
+                val = false;
+                break;
+            }
+        }
+    }
+
+    return val;
+}
+
+function transivity(){
+    var matrix = matrixInput();
+    var val = true;
+    for(i = 0; i < matrix.length; i++){
+        for(j = 0; j < matrix.length; j++){
+            for(c = 0; c < matrix.length; c++){
+                if(matrix[i][c] && matrix[c][j] && !matrix[i][j]){
+                    val = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    return val;
+}
+console.log(transivity());
