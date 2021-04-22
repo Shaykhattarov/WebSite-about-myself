be38c8c4fd2c8fd018c6eea9a5bfd933e5696f66
Ildan Shaykhattarov <ildan201@yandex.ru>
2021-04-22 14:41:48 +0300

    Финальная версия

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var result = 0;
 function Create2DArray(length) {
     var arr = [];
   
@@ -29,10 +30,8 @@ function matrixInput(){
             coun++;
         }
     }
-
     return matrix;
 }
-
 
 function reflexion(){
     var matrix = matrixInput();
@@ -44,9 +43,13 @@ function reflexion(){
             break;
         }
     }
-    return val;
+    if (val == true){
+        result = "Матрица рефлексивна";
+    }
+    else {
+        result = "Матрица не рефлексивна";
+    }
 }
-
 
 function symmetry(){
     var matrix = matrixInput();
@@ -60,23 +63,134 @@ function symmetry(){
         }
     }
 
-    return val;
+
+    if(val == true && matrix[0][0] == 1){
+        result += ", симметрична";
+    }
+    else {
+        result += ", не симметрична";
+    }
 }
 
-function transivity(){
-    var matrix = matrixInput();
+function antisymmetry(){
+    var matrixA = matrixInput();
+    var matrixB = matrixInput();
+    var matrixC = matrixInput();
+    var N = matrixA.length;
     var val = true;
-    for(i = 0; i < matrix.length; i++){
-        for(j = 0; j < matrix.length; j++){
-            for(c = 0; c < matrix.length; c++){
-                if(matrix[i][c] && matrix[c][j] && !matrix[i][j]){
-                    val = false;
-                    break;
-                }
+    
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++) {
+            matrixC[i][j]=0;
+            for (k = 0; k < N; k++){
+               matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
             }
         }
     }
 
-    return val;
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++) {
+            if(matrixC[i][i] != matrixC[0][0] || (i != j && matrixC[i][j] != 0)) {
+                val = false;
+                break;
+            }
+        }
+    }
+
+    if(val == true){
+        result += ", антисимметрична";
+    }
+    else{
+        result += ", не антисимметрична";
+    }
 }
-console.log(transivity());
+
+
+function transivity(){
+    var matrixA = matrixInput();
+    var matrixB = matrixInput();
+    var matrixC = matrixInput();
+    var N = matrixA.length;
+    var val = true;
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++) {
+            matrixC[i][j]=0;
+            for (k = 0; k < N; k++){
+               matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+               if(matrixC[i][j] == 2){
+                   matrixC[i][j] = 1;
+               }
+            }
+        }
+    }
+
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++) {
+            if(matrixA[i][j] != matrixC[i][j]){
+                val = false;
+                break;
+            }
+        }
+    }
+
+    if(val == true){
+        result += " и транзитивна.";
+    }
+    else{
+        result += " и не транзитивна.";
+    }
+}
+
+function validation(){
+    var length = document.getElementById("rows").value;
+    var matrix = matrixInput();
+    var val = true, val_matrix = true;
+    
+    for(i = 0; i < length; i++){
+        if (matrix[i].length == length){
+            val = true;
+        }
+        else{
+            val = false;
+            break;
+        }
+    }
+    
+    for(i = 0; i < length; i++){
+        for(j = 0; j < length; j++){
+            if(matrix[i][j] == 0 || matrix[i][j] == 1){
+                val_matrix = true;
+            }
+            else{
+                val_matrix = false;
+                break;
+            }
+        }
+    }
+
+    if(val == true && val_matrix == true)
+    {
+        reflexion();
+        antisymmetry();
+        symmetry();
+        transivity();
+    }
+    else if(val == true && val_matrix != true){
+        alert("Матрица может состоять только из 0 и 1!");
+        val_matrix = true;
+    }
+    else if(val != true && val_matrix == true){
+        alert("Не хватает данных в матрице!");
+        val = true;
+    }
+    else{
+        alert("При вводе матрицы были совершены ошибки!");
+        val = true;
+        val_matrix = true;
+    }
+}
+
+document.getElementById("resultButton").onclick = function() {
+    validation();
+    document.getElementById('resultOut').value = result;
+};
